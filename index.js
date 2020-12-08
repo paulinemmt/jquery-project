65ecdbed27448eb5a9f5e96637c25858953add85
Pauline Momot <pauline.momot@gmail.com>
2020-12-08 14:50:28 +0100

    mise en place de la première partie du html

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 //     $("#p1").click( function() {
 //             $("#p5").fadeTo(1000, 0.7);
 //     });
+// // });
+
+// $(function () {
+
+//     $("button").click( function() {
+//             $("div").animate({left: "600px",
+//         opacity: 0.5,
+//         width: "+=150px",
+//         }, 3000, function() {
+// $('div').hide()
+//             }            );
+//     });
 // });
-
-$(function () {
-
-    $("button").click( function() {
-            $("div").animate({left: "600px",
-        opacity: 0.5,
-        width: "+=150px",
-        }, 3000, function() {
-$('div').hide()
-            }            );
-    });
-});
