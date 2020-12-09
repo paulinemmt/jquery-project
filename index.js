553da1c0d99d295fc113f0f7227d7bb1f38f49ad
Pauline Momot <pauline.momot@gmail.com>
2020-12-09 16:23:15 +0100

    Mise en place des animations d'ouverture des items

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-// $(function () {
+$(function () {
+    console.log("hey")
+// Elément du menu
+    let $mainMenuItems = $("#main-menu ul").children("li"), //sélection de tous les éléments li
+        totalMainMenuItems = $mainMenuItems.length, //nombre d'éléments li
+        openedIndex = -1, //index de départ pour que tous les menus soient fermés
 
-//     $("#p1").click( function() {
-//             $("#p5").fadeTo(1000, 0.7);
-//     });
-// // });
+        //Passage de l'image colorée sur l'image bw au moment du click
+        init = function () {
+            bindEvents()
+        };
 
-// $(function () {
+        //Fonction relatif à l'évènement du click
+        bindEvents = function () {
 
-//     $("button").click( function() {
-//             $("div").animate({left: "600px",
-//         opacity: 0.5,
-//         width: "+=150px",
-//         }, 3000, function() {
-// $('div').hide()
-//             }            );
-//     });
-// });
+            $mainMenuItems.children(".images").click(function(){
+
+                let newIndex = $(this).parent().index(),
+                    $item = $mainMenuItems.eq(newIndex);
+
+                    if (openedIndex == newIndex){
+                        animateItem($item, false, 250);
+                        openedIndex = -1
+                    }else {
+                        if(validIndex(newIndex)){
+                        animateItem($mainMenuItems.eq(openedIndex), false, 250);
+                        openedIndex = newIndex;
+                        animateItem($item, true, 250)
+                        }
+                    }
+                
+                openedIndex = newIndex;
+
+            });
+        }
+
+        //Vérification de la validité de l'index
+        validIndex = function(indexToCheck){
+            return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems)
+        }
+
+
+        //Anmation ouvrir/fermer les infos
+        animateItem = function($item, toOpen, speed){
+
+            let $colorImage = $item.find(".color"),
+                itemParam = toOpen ? {width: "420px"}:{width: "140px"}, //?: vrai ou faux ?
+                colorImageParam = toOpen ? {left: "0px"} : {left: "140px"};
+
+            $colorImage.animate(colorImageParam, speed); //décale l'image colorée sur l'image en bw
+            $item.animate(itemParam, speed); //ouvre la description
+        }
+
+        init();
+})
